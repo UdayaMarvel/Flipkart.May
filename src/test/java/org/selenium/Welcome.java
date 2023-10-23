7612aebb2b9fecf1f9e2f299970f3df087989867
Elavarasan <elavarasan0411@gmail.com>
2023-10-23 08:03:24 +0530

    Added new class

---
+++ src/test/java/org/selenium/Welcome.java
@@ -0,0 +1,8 @@
+public class Main {
+    public void elava() {
+        System.out.println('Welcome !!');
+    }
+    public static void main(String[] args) {}
+    Main ref = new Main();
+    ref.elava();
+}
